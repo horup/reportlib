cc914b4e37d4021f0d8689873b3cbf80af308156
horup <sah@sah.dk>
2020-03-14 20:35:09 +0100

    added react and email

--- index.tsx
+++ index.tsx
@@ -1,5 +1,27 @@
+import * as React from 'react';
+import * as ReactDOMServer from 'react-dom/server';
+import * as sgMail from '@sendgrid/mail';
 
-export class ReportLib
+export function renderToString(element:React.ReactElement<any>)
 {
-    
+    let html = ReactDOMServer.renderToString(element);
+    return html;
 }
+
+export function sendEmail(api:string, from:string, to:string[], title:string, body:string)
+{
+    sgMail.setApiKey(api);
+    sgMail.sendMultiple(
+        {
+            to:to,
+            from:"setireport@farmonline.net", 
+            html:body,
+            personalizations:[{to:to}],
+            subject:title
+        }).then(()=>
+        {
+            console.log("mail sent!");
+        });  
+}
+
+export {React};
