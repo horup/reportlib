e3dffd50e91c71fabb0052f7390f4c5f1ae2e82f
horup <sah@sah.dk>
2020-03-15 20:29:08 +0100

    refactored

--- index.tsx
+++ index.tsx
@@ -2,26 +2,75 @@ import * as React from 'react';
 import * as ReactDOMServer from 'react-dom/server';
 import * as sgMail from '@sendgrid/mail';
 
-export function renderToString(element:React.ReactElement<any>)
+export interface IEmailConfig
 {
-    let html = ReactDOMServer.renderToString(element);
-    return html;
+    api:string;
+    from:string;
+    simulate?:boolean;
+}
+export interface IConfig
+{
+    email:IEmailConfig;
 }
 
-export function sendEmail(api:string, from:string, to:string[], title:string, body:string)
+export class ReportLib
 {
-    sgMail.setApiKey(api);
-    sgMail.sendMultiple(
+    config:IConfig =
+    {
+        email:
         {
-            to:to,
-            from:"setireport@farmonline.net", 
-            html:body,
-            personalizations:[{to:to}],
-            subject:title
-        }).then(()=>
+            api:"",
+            from:""
+        }
+    }
+
+    setEmailConfig(emailConfig:IEmailConfig)
+    {
+        this.config.email = emailConfig;
+        return this;
+    }
+
+    sendEmail(to:string[], title:string, body:string|React.ReactElement<any>)
+    {
+        if (typeof body == "object")
         {
-            console.log("mail sent!");
-        });  
+            body = this.renderToString(body);
+        }
+        if (this.config.email.api == "" || this.config.email.from == "")
+        {
+            throw "api or from is empty";
+        }
+
+        if (this.config.email.simulate == true)
+        {
+            console.log("--------------------");
+            console.log("Title: " + title);
+            console.log( "From: " + this.config.email.from);
+            console.log( "  To: " + to);
+            console.log(body);
+            console.log("--------------------");
+            return;
+        }
+
+        sgMail.setApiKey(this.config.email.api);
+        sgMail.sendMultiple(
+            {
+                to:to,
+                from:this.config.email.from, 
+                html:body,
+                personalizations:[{to:to}],
+                subject:title
+            }).then(()=>
+            {
+                console.log("mail sent!");
+            });  
+    }
+
+    renderToString(element:React.ReactElement<any>)
+    {
+        let html = ReactDOMServer.renderToString(element);
+        return html;
+    }
 }
 
 export {React};
